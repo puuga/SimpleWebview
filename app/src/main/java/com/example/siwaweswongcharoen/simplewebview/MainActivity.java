7e6eb5395786892b7a27db07fec191cd74a88d0b
puuga <siwaoh@gmail.com>
2014-08-21 21:27:02 +0700

    update feature

--- app/src/main/java/com/example/siwaweswongcharoen/simplewebview/MainActivity.java
+++ app/src/main/java/com/example/siwaweswongcharoen/simplewebview/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.siwaweswongcharoen.simplewebview;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
@@ -25,21 +30,35 @@ public class MainActivity extends Activity {
         configWebView();
     }
 
+    @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface"})
     private void configWebView() {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setGeolocationEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new MyWebClient(getApplicationContext()));
+        webView.addJavascriptInterface((this), "Android");
         webView.loadUrl(webURL);
     }
 
     @Override
     public void onBackPressed() {
-        if(webView.canGoBack() == true){
+        if (webView.canGoBack()) {
             webView.goBack();
-        }else{
+        } else {
             super.onBackPressed();
         }
+    }
+
+    @JavascriptInterface
+    public void showNotification(String title, String content) {
+        //Log.d("show noti", "show noti:" + title + "," + content);
+        NotificationCompat.Builder mBuilder;
+        mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.ic_launcher).setContentTitle(title).setContentText(content);
+        Notification notification = mBuilder.build();
+        notification.defaults = Notification.DEFAULT_ALL;
+        int mNotificationId = 1;
+        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, notification);
     }
 
 }
